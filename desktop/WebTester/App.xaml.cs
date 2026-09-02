fad09258c18e85e9cb5e286d382135ad462b4292
Shane McCaw <smccaw@mccawsoft.com>
2026-09-02 12:42:57 -0400

    Scaffold desktop/WebTester project shell

---
+++ desktop/WebTester/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WebTester;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
