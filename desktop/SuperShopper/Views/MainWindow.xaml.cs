dd5ee9aa7e0992fe86e9d7fa56829d74321d7e6a
Shane McCaw <smccaw@mccawsoft.com>
2026-09-08 21:24:11 -0400

    Fix maximized window taskbar overlap by hooking Win32 WM_GETMINMAXINFO monitor work area bounds

--- desktop/SuperShopper/Views/MainWindow.xaml.cs
+++ desktop/SuperShopper/Views/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Interop;
 using Microsoft.Web.WebView2.Core;
 using SuperShopper.ViewModels;
 
@@ -11,6 +13,48 @@ namespace SuperShopper.Views
         public MainWindow()
         {
             InitializeComponent();
+            SourceInitialized += MainWindow_SourceInitialized;
+        }
+
+        private void MainWindow_SourceInitialized(object? sender, EventArgs e)
+        {
+            var handle = new WindowInteropHelper(this).Handle;
+            HwndSource.FromHwnd(handle)?.AddHook(WindowProc);
+        }
+
+        private IntPtr WindowProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == 0x0024) // WM_GETMINMAXINFO
+            {
+                WmGetMinMaxInfo(hwnd, lParam);
+                handled = true;
+            }
+            return IntPtr.Zero;
+        }
+
+        private static void WmGetMinMaxInfo(IntPtr hwnd, IntPtr lParam)
+        {
+            var mmi = Marshal.PtrToStructure<MINMAXINFO>(lParam);
+
+            var currentMonitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+            if (currentMonitor != IntPtr.Zero)
+            {
+                var monitorInfo = new MONITORINFO();
+                monitorInfo.cbSize = Marshal.SizeOf(typeof(MONITORINFO));
+                GetMonitorInfo(currentMonitor, ref monitorInfo);
+
+                var rcWorkArea = monitorInfo.rcWork;
+                var rcMonitorArea = monitorInfo.rcMonitor;
+
+                mmi.ptMaxPosition.X = Math.Abs(rcWorkArea.Left - rcMonitorArea.Left);
+                mmi.ptMaxPosition.Y = Math.Abs(rcWorkArea.Top - rcMonitorArea.Top);
+                mmi.ptMaxSize.X = Math.Abs(rcWorkArea.Right - rcWorkArea.Left);
+                mmi.ptMaxSize.Y = Math.Abs(rcWorkArea.Bottom - rcWorkArea.Top);
+                mmi.ptMaxTrackSize.X = mmi.ptMaxSize.X;
+                mmi.ptMaxTrackSize.Y = mmi.ptMaxSize.Y;
+            }
+
+            Marshal.StructureToPtr(mmi, lParam, true);
         }
 
         private async void Window_Loaded(object sender, RoutedEventArgs e)
@@ -29,15 +73,13 @@ namespace SuperShopper.Views
         {
             if (WindowState == WindowState.Maximized)
             {
-                // When maximized in WindowChrome, add padding so the window stays strictly within screen work area (doesn't cover Windows taskbar/Start menu)
-                mainBorder.Margin = new Thickness(
-                    SystemParameters.WindowResizeBorderThickness.Left + 2,
-                    SystemParameters.WindowResizeBorderThickness.Top + 2,
-                    SystemParameters.WindowResizeBorderThickness.Right + 2,
-                    SystemParameters.WindowResizeBorderThickness.Bottom + 2);
+                // Slight border adjustment to ensure border rendering stays clean when maximized
+                mainBorder.BorderThickness = new Thickness(0);
+                mainBorder.Margin = new Thickness(0);
             }
             else
             {
+                mainBorder.BorderThickness = new Thickness(1);
                 mainBorder.Margin = new Thickness(0);
             }
         }
@@ -133,5 +175,50 @@ namespace SuperShopper.Views
                 }
             }
         }
+
+        #region Win32 P/Invoke Definitions
+        private const int MONITOR_DEFAULTTONEAREST = 0x00000002;
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr MonitorFromWindow(IntPtr handle, int flags);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFO lpmi);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct MINMAXINFO
+        {
+            public POINT ptReserved;
+            public POINT ptMaxSize;
+            public POINT ptMaxPosition;
+            public POINT ptMinTrackSize;
+            public POINT ptMaxTrackSize;
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        private struct MONITORINFO
+        {
+            public int cbSize;
+            public RECT rcMonitor;
+            public RECT rcWork;
+            public int dwFlags;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+        #endregion
     }
 }
